c2c80f8e9c204318371daa8dfa8417249e013ab3
Akshay <Akshay>
2016-02-11 23:33:05 +0530

    Basic Audio and video steganography done.

---
+++ main/video_embedder.py
@@ -0,0 +1,67 @@
+import cv2
+import numpy as np
+from PIL import Image
+import stepic
+import random
+from math import ceil, floor
+from Crypto.Cipher import AES
+import binascii
+import sys
+import os
+import base64
+import ifvm_helper as ifvm
+
+def main():
+	occ = []
+	cover = sys.argv[1]
+	secret = sys.argv[2]
+	embed(cover, secret, occ)
+
+def embed(cover, secret, occ, saveLocation = "output.avi"):
+	'''Embeds  images. Call this function when the module is imported
+	cover = Cover Video Path/Location
+	secret = secret Video Path/Location'''
+
+	# Opening video & extracting frames
+	cap = cv2.VideoCapture(cover)
+	frameCount = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+	fps = cap.get(cv2.CAP_PROP_FPS)
+	frames = ifvm.getAllFrames(cap)
+
+	# Opening secret Image and converting to string for embedding
+	secret_video_text_file_path = "secret_video_file_154861.txt"
+	with open(secret, "rb") as videoFile:
+		vid_data = base64.b64encode(videoFile.read())
+
+	with open(secret_video_text_file_path, "w") as txt:
+		txt.write(vid_data)
+
+	# define capacity of each data frame
+	h, w = frames[0].shape[:2]
+	pixelCount = h * w - 1
+	byteCapacity = pixelCount / 9 # defines maximum no. of bytes that can be stored in an image (Ref. http://domnit.org/blog/2007/02/stepic-explanation.html)
+
+	blocks = ifvm.generateTextBlocks(secret_video_text_file_path, byteCapacity)
+	os.remove(secret_video_text_file_path)
+	index = ifvm.generateRandomFrameNo(len(frames), occ)
+	key = ifvm.generateIndexHash(index)
+
+	# generating data that will be encoded in index frame
+	indexData = ifvm.generateIndexData('output.txt', secret.split('.')[-1])
+
+	for block in blocks:
+		loc = ifvm.generateRandomFrameNo(len(frames), occ)
+		indexData += str(loc) + '.'
+		img = Image.fromarray(frames[loc])
+		stegimg = stepic.encode(img, block)
+		frames[loc] = np.array(stegimg)
+
+	# steganography on index frame
+	print 'Index Frame Data: ', indexData
+	img = Image.fromarray(frames[index])
+	stegimg = stepic.encode(img, indexData)
+	frames[index] = np.array(stegimg)
+	ifvm.writeToVideo(frames, saveLocation, fps)
+
+if __name__ == '__main__':
+	main()
---
+++ main/video_extractor.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+from PIL import Image
+import stepic
+import random
+from math import ceil, floor
+from Crypto.Cipher import AES
+import binascii
+import sys
+import os
+import base64
+import ifvm_helper as ifvm
+
+def main():
+	cover = sys.argv[1]
+	input_hash = raw_input('Enter the key for your data: ')
+	extract(cover, input_hash)
+
+def extract(cover, key):
+	cap = cv2.VideoCapture(cover)
+	index = int(ifvm.decodeIndexHash(binascii.unhexlify(key))) # <- Index at which our data is hidden
+
+	frameCount = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+	frames = ifvm.getAllFrames(cap)
+
+	img = Image.fromarray(frames[index])
+	indexData = stepic.decode(img).decode()
+	indexData = indexData.split(';')
+
+	video_txt_location = 'outfile.' + indexData[0]
+	video_location = 'extractedVideo.' + indexData[1]
+
+	f = open(video_txt_location, 'w')
+
+	dataLocs = indexData[2].split('.')
+
+	for loc in dataLocs:
+		if loc == '': break
+		img = Image.fromarray(frames[int(loc)])
+		block = stepic.decode(img).decode()
+		f.write(block)
+	f.close()
+
+	with open(video_txt_location,"rb") as res:
+		str = base64.b64decode(res.read())
+	fh = open(video_location, "wb")
+	fh.write(str)
+	fh.close()
+	os.remove(video_txt_location)
+
+
+if __name__ == '__main__':
+	main()
