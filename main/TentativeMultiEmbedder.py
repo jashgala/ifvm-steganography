080abfb497e4574ff84bbff79ec0344655be42fd
Akshay <Akshay>
2016-05-02 20:04:06 +0530

    Changes

--- main/TentativeMultiEmbedder.py
+++ main/TentativeMultiEmbedder.py
@@ -1,4 +1,3 @@
-from __future__ import print_function
 import cv2
 import numpy as np
 from PIL import Image
@@ -11,31 +10,13 @@ import sys
 import os
 import base64
 import ifvm_helper as ifvm
-import subprocess
 import media_embedder as embd
 
-def main():
-	occ = []
-	cover = raw_input("Enter the cover video for Steganography: ")
-	original_cover = cover
-	count = int(raw_input("Enter the number of files you want to steganograph to steganograph: "))
-	for i in range(count):
-		secret = raw_input("Enter the file name of the media you want to embed in the video: ")
-		cover, occ = embd.embed(cover, secret, occ, saveLocation = "output.avi")
-
-	# print("Transcribing audio")
-	# command = ["ffmpeg" , "-i" , original_cover , "-vn" , "-acodec" , "copy" , "output-audio.aac"]
-	# subprocess.call(command)
-	# command = ["ffmpeg" , "-i" , "output.avi" , "-i" , "output-audio.aac" , "-vcodec" , "copy" , "-acodec" , "copy" , "muxed_file.avi"]
-	# subprocess.call(command)
-
-
-def embed_multiple(cover, secret, occ, saveLocation):
-	original_cover = cover
-	for i in range(count):
-		secret = raw_input("Enter the file name of the media you want to embed in the video: ")
-		cover, occ = embd.embed(cover, secret, occ, saveLocation = "output.avi")
-
-if __name__ == '__main__':
-	main()
+occ = []
+cover = raw_input("Enter the cover video for Steganography: ")
+count = int(raw_input("Enter the number of files you want to steganograph to steganograph: "))
+for i in range(count):
+	secret = raw_input("Enter the file name of the media you want to embed in the video: ")
+	occ = embd.embed(cover, secret, occ, saveLocation = "output.avi")
+	
 	
--- main/media_embedder.py
+++ main/media_embedder.py
@@ -1,7 +1,3 @@
-from Tkinter import *
-import tkMessageBox
-import os
-
 import cv2
 import numpy as np
 from PIL import Image
@@ -27,13 +23,11 @@ def embed(cover, secret, occ, saveLocation = "output.avi"):
 	secret = secret Media Path/Location
 	occ = List of occupied frames'''
 
-	global key
 	# Opening video & extracting frames
 	cap = cv2.VideoCapture(cover)
-	frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+	frameCount = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 	fps = cap.get(cv2.CAP_PROP_FPS)
 	frames = ifvm.getAllFrames(cap)
-	not_occupied = [i for i in range(frameCount) if i not in occ]
 
 	# Opening secret Image and converting to string for embedding
 	secret_media_text_file_path = "secret_file_154861.txt"
@@ -46,18 +40,18 @@ def embed(cover, secret, occ, saveLocation = "output.avi"):
 	# define capacity of each data frame
 	h, w = frames[0].shape[:2]
 	pixelCount = h * w - 1
-	byteCapacity = pixelCount / 3 # defines maximum no. of bytes that can be stored in an image (Ref. http://domnit.org/blog/2007/02/stepic-explanation.html)
+	byteCapacity = pixelCount / 9 # defines maximum no. of bytes that can be stored in an image (Ref. http://domnit.org/blog/2007/02/stepic-explanation.html)
 
 	blocks = ifvm.generateTextBlocks(secret_media_text_file_path, byteCapacity)
 	os.remove(secret_media_text_file_path)
-	index = ifvm.generateRandomFrameNo(len(frames), occ, not_occupied)
+	index = ifvm.generateRandomFrameNo(len(frames), occ)
 	key = ifvm.generateIndexHash(index)
 
 	# generating data that will be encoded in index frame
 	indexData = ifvm.generateIndexData('output.txt', secret.split('.')[-1])
 
 	for block in blocks:
-		loc = ifvm.generateRandomFrameNo(len(frames), occ, not_occupied)
+		loc = ifvm.generateRandomFrameNo(len(frames), occ)
 		indexData += str(loc) + '.'
 		img = Image.fromarray(frames[loc])
 		stegimg = stepic.encode(img, block)
@@ -68,8 +62,8 @@ def embed(cover, secret, occ, saveLocation = "output.avi"):
 	img = Image.fromarray(frames[index])
 	stegimg = stepic.encode(img, indexData)
 	frames[index] = np.array(stegimg)
-	saveLocation = ifvm.writeToVideo(frames, saveLocation, fps)
-	return (saveLocation, occ)
+	ifvm.writeToVideo(frames, saveLocation, fps)
+	return occ
 
 if __name__ == '__main__':
 	main()
